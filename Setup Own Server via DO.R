a2d41a20b7734fbfaf5a3e05cd83937492772e0a
®γσ ξηg <englianhu@gmail.com>
2021-09-11 17:37:32 +0900

    update

--- Setup Own Server via DO.R
+++ Setup Own Server via DO.R
@@ -2,12 +2,14 @@
 1) Create a droplet
 2) 
 3) 
-sudo apt-get update && sudo apt-get install lsb-core
+
+sudo apt update && sudo apt install lsb-core
 sudo lsb_release
 sudo apt install -y nginx
 sudo apt install -y firewalld
-sudo systemctl enable firewalld
-sudo systemctl start firewall
-sudo firewall-cmd --list-all
+#sudo systemctl enable firewalld
+#sudo systemctl start firewall
+#sudo firewall-cmd --list-all
+sudo apt -y install gdebi-core
 
 
